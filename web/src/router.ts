7c57521465bb75442abfa9b05f5da6c8cba154d6
二刺螈 <i@songe.li>
2025-11-24 19:55:34 +0800

    fix: 404 page

--- web/src/router.ts
+++ web/src/router.ts
@@ -1,14 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router';
 
-const routes = [
-  { path: '/', component: () => import('./views/HomePage.vue') },
-  {
-    path: '/:pathMatch(.*)*',
-    component: import('./views/404Page.vue'),
-  },
-];
-
 export default createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', component: () => import('./views/HomePage.vue') },
+    {
+      path: '/:pathMatch(.*)*',
+      component: () => import('./views/404Page.vue'),
+    },
+  ],
 });
--- web/src/views/404Page.vue
+++ web/src/views/404Page.vue
@@ -1,3 +1,16 @@
 <template>
-  <div text-center pt-120px>404 Not Found</div>
+  <div flex flex-col items-center pt-120px gap-16px>
+    <div>404 Not Found</div>
+    <a
+      href="/"
+      px-4px
+      rounded-md
+      border
+      border-gray-300
+      text-gray-600
+      hover="color-[rgb(from_currentColor_r_g_b_/_50%)] bg-gray-100"
+    >
+      Home
+    </a>
+  </div>
 </template>
